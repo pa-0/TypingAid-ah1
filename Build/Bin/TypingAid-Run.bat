6e409ed2ab29d357bb5804200772f1bdc29dedf2
Alex Fainshtein <alex.fainshtein@bd.com>
2018-03-17 18:47:42 -0700

    Nothing important

--- Build/Bin/TypingAid-Run.bat
+++ Build/Bin/TypingAid-Run.bat
@@ -1,4 +1,9 @@
 REM Starting TypingAid without compilation. I can try to attach VS later.
 cd C:\GitHub\TypingAid\Build\Bin
+REM If we want cmd window to stay open after AutoHotkeyU64.exe exits
 C:\Windows\System32\cmd.exe /k C:\Users\10114976\Downloads\Apps\AutoHotKey\AutoHotkey_1.1.28.00\AutoHotkeyU64.exe^
+ ..\..\Source\TypingAid.ahk
+
+REM Console window will close after AutoHotkeyU64.exe is killed; closing console will kill the app
+C:\Users\10114976\Downloads\Apps\AutoHotKey\AutoHotkey_1.1.28.00\AutoHotkeyU64.exe^
  ..\..\Source\TypingAid.ahk
