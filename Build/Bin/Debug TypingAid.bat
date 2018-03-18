afc4695f18930aacf74a747b5f57c1c7ac962d50
Alex Fainshtein <alex.fainshtein@bd.com>
2018-03-17 20:48:37 -0700

    Update and rename TypingAid-Debug.bat to Debug TypingAid.bat

---
+++ Build/Bin/Debug TypingAid.bat
@@ -0,0 +1,13 @@
+REM Starting in Debugging mode - use it to debug with Notepad++ (or another DBGp client)
+cd C:\GitHub\TypingAid\Build\Bin
+
+REM If we want cmd window to stay open after AutoHotkeyU64.exe exits:
+
+REM C:\Windows\System32\cmd.exe /k C:\Users\10114976\Downloads\Apps\AutoHotKey\AutoHotkey_1.1.28.00\AutoHotkeyU64.exe^
+REM  /Debug^
+REM  ..\..\Source\TypingAid.ahk	
+ 
+REM Console window will close after AutoHotkeyU64.exe is killed; closing console will NOT kill the app
+
+C:\Users\10114976\Downloads\Apps\AutoHotKey\AutoHotkey_1.1.28.00\AutoHotkeyU64.exe^
+  ..\..\Source\TypingAid.ahk
