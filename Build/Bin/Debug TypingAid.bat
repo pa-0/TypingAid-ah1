47f636c0bb9e97e68ca6f1ecb1143921affe8111
Alex Fainshtein <alex.fainshtein@bd.com>
2018-03-17 20:49:51 -0700

    Update Debug TypingAid.bat

--- Build/Bin/Debug TypingAid.bat
+++ Build/Bin/Debug TypingAid.bat
@@ -4,10 +4,9 @@ cd C:\GitHub\TypingAid\Build\Bin
 REM If we want cmd window to stay open after AutoHotkeyU64.exe exits:
 
 REM C:\Windows\System32\cmd.exe /k C:\Users\10114976\Downloads\Apps\AutoHotKey\AutoHotkey_1.1.28.00\AutoHotkeyU64.exe^
-REM  /Debug^
-REM  ..\..\Source\TypingAid.ahk	
+REM  /Debug  ..\..\Source\TypingAid.ahk	
  
 REM Console window will close after AutoHotkeyU64.exe is killed; closing console will NOT kill the app
 
 C:\Users\10114976\Downloads\Apps\AutoHotKey\AutoHotkey_1.1.28.00\AutoHotkeyU64.exe^
-  ..\..\Source\TypingAid.ahk
+   /Debug ..\..\Source\TypingAid.ahk
